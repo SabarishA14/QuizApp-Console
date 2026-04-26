2ef6bbcaa7231a8f6be4cb78ad11ebca73fc4320
SabarishA14 <sabarish.a14@gmail.com>
2026-04-26 11:54:23 +0530

    Initial commit

---
+++ src/main/java/com/sab/Main.java
@@ -0,0 +1,10 @@
+package com.sab;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        QuestionService qs = new QuestionService();
+        qs.playQuiz();
+
+    }
+}
